3ccc74b2ee4072c5dc5b71586a825f98dc50f771
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-05-10 11:41:52 +0200

    11:41

--- kerites_Zoli/Program.cs
+++ kerites_Zoli/Program.cs
@@ -43,6 +43,94 @@ namespace kerites_Zoli
                 }
                 n++;
             }
+
+            //2.feladat
+            Console.WriteLine("2.feladat\nAz eladott telkek száma: {0}", n);
+
+            //3.a feladat
+            if (adatok[n-1].paros_paratlan == 0)
+            {
+                Console.WriteLine("3.feladat\nA páros oldalon adták el az utolsó telket.");
+            }
+            else
+            {
+                Console.WriteLine("3.feladat\nA páratlan oldalon adták el az utolsó telket.");
+            }
+
+            //3.b feladat
+            Console.WriteLine("Az utolsó telek házszáma: {0}",adatok[n-1].hazszam);
+
+            //4.feladat
+            string szin1 = null;
+            string szin2 = null;
+            int szamlalo = 0;
+            int k = 0;
+            for (int i = 0;i<n;i++)
+            {
+                if (adatok[i].paros_paratlan == 1)
+                {
+                    szamlalo++;
+                    if (adatok[i].k_szin != ":" && adatok[i].k_szin != "#" && szamlalo == 1)
+                    {
+                        szin1 = adatok[i].k_szin;
+                    }
+                    if (adatok[i].k_szin != ":" && adatok[i].k_szin != "#" && szamlalo == 2)
+                    {
+                        szin2 = adatok[i].k_szin;
+                        if (szin1 == szin2)
+                        {
+                            Console.WriteLine("4.feladat\nA szomszédossal egyezik a kerítés színe: {0}", adatok[k].hazszam);
+                            break;
+                        }
+                        szin1 = szin2;                       
+                    }                   
+                }
+                szamlalo = 1;
+                k = i;
+            }
+
+            //5.a feladat
+            Console.Write("5.feladat:\nAdjon meg egy házszámot!  ");
+            int adott_hsz =int.Parse(Console.ReadLine());
+            string hazszin = null;
+            for (int i =0; i<n;i++)
+            {
+                if (adott_hsz == adatok[i].hazszam)
+                {
+                    if (adatok[i].k_szin !=":" && adatok[i].k_szin != "#")
+                    {
+                        Console.WriteLine("A kerítés színe: {0}", adatok[i].k_szin);
+                        hazszin = adatok[i].k_szin;
+                    }
+                    else
+                    {
+                        Console.WriteLine("A kerítés állapota: {0}", adatok[i].k_szin);
+                        hazszin = adatok[i].k_szin;
+                    }
+                }
+            }
+
+            //5.b feladat
+            for (int i ='A';i<='Z';i++)
+            {
+                for (int j = 0;j<n;j++)
+                {
+                    if (adatok[j].hazszam == adott_hsz - 2)
+                    {
+                        szin1 = adatok[j].k_szin;
+                    }
+                    if (adatok[j].hazszam == adott_hsz + 2)
+                    {
+                        szin2 = adatok[j].k_szin;
+                    }
+                }
+                if (Convert.ToChar(szin1) != Convert.ToChar(i) && Convert.ToChar(szin2) != Convert.ToChar(i) && Convert.ToChar(hazszin) != Convert.ToChar(i))
+                {
+                    Console.WriteLine("Egy lehetséges festési szín: {0}", Convert.ToChar(i));
+                    break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
